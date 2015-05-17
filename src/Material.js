aecc4a028a764910dfee9eeeab68cd8c41bff183
Redcamel <webseon@gmail.com>
2015-05-17 14:51:40 +0900

    #73 setBackgroundColor

--- src/Material.js
+++ src/Material.js
@@ -5,7 +5,6 @@ var Material = (function () {
     var Material, fn;
     var hex = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i, hex_s = /^#?([a-f\d]{1})([a-f\d]{1})([a-f\d]{1})$/i;
     Material = function Material() {
-        var t0 = arguments[0], t1, ta
         this._textures = {},
         this._diffuse = {__indexList: []},
         this._specular = {__indexList: []},
@@ -19,29 +18,33 @@ var Material = (function () {
         this._count = 0,
         this._scene = null,
         this._key = null
+        this.setBackgroundColor.apply(this, arguments)
+    },
+    fn = Material.prototype,
+    fn.setBackgroundColor=function setBackgroundColor(){
+        var t0 = arguments[0], t1, ta
         if (arguments.length == 1) {
             if (t0.length > 7) ta = +t0.substr(7), t0 = t0.substr(0, 7)
             if (t0.charAt(0) == '#') {
                 if (t1 = hex.exec(t0)) {
                     this._r = parseInt(t1[1], 16) / 255,
-                    this._g = parseInt(t1[2], 16) / 255,
-                    this._b = parseInt(t1[3], 16) / 255
+                        this._g = parseInt(t1[2], 16) / 255,
+                        this._b = parseInt(t1[3], 16) / 255
                 } else {
                     t1 = hex_s.exec(t0),
-                    this._r = parseInt(t1[1] + t1[1], 16) / 255,
-                    this._g = parseInt(t1[2] + t1[2], 16) / 255,
-                    this._b = parseInt(t1[3] + t1[3], 16) / 255
+                        this._r = parseInt(t1[1] + t1[1], 16) / 255,
+                        this._g = parseInt(t1[2] + t1[2], 16) / 255,
+                        this._b = parseInt(t1[3] + t1[3], 16) / 255
                 }
                 this._a = ta ? ta > 1 ? 1 : ta : 1
             }
         } else {
             this._r = arguments[0],
-            this._g = arguments[1],
-            this._b = arguments[2],
-            this._a = arguments[3] ? arguments[3] : 1
+                this._g = arguments[1],
+                this._b = arguments[2],
+                this._a = arguments[3] ? arguments[3] : 1
         }
     },
-    fn = Material.prototype,
     fn.addTexture = function addTexture(type,textureID/*,index,blendMode*/) {
         var t = this._scene
         if (t && !t._textures[textureID]) MoGL.error('Material', 'addTexture', 0)
