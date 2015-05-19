7c257de64cdc07aaa28b66efe79537da79c646e0
hanmomhanda@naver.com <hanmomhanda@naver.com>
2015-05-20 02:11:19 +0900

    Scene.js 내 makeTexture 함수 내 이미지 2의 거듭제곱 부분 보완

--- src/Scene.js
+++ src/Scene.js
@@ -303,25 +303,21 @@ var Scene = (function () {
 			// gl.generateMipmap(gl.TEXTURE_2D)
 			// gl.bindTexture(gl.TEXTURE_2D, null)
 			// texture.loaded=1
-console.log('&&&&&&&&&&&&&&&&&&&&&&&&&& hanmomhanda &&&&&&&&&&&&&&&&&&&&&&&&&&');
+
 			var isPowerOf2 = function(value) {
 				return (value & (value - 1)) == 0;
 			};
 			gl.bindTexture(gl.TEXTURE_2D, texture),
 			//TODO 다변화 대응해야됨
 			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.img);
-			//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
 			if (isPowerOf2(texture.img.width) && isPowerOf2(texture.img.height)) {
-console.log('isPowerOf2');
-                gl.generateMipmap(gl.TEXTURE_2D);
-            } else {
-console.log('not PowerOf2');
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE),
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE),
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-            }
-			// gl.bindTexture(gl.TEXTURE_2D, null)
-			texture.loaded=1
+				gl.generateMipmap(gl.TEXTURE_2D);
+			} else {
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE),
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE),
+				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+			}
+			texture.loaded = 1;
 		}
 		texture.data = image
 		self._glTEXTUREs[id] = texture
