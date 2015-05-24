555e70f4107d0dece42282a7ad1e12bf01b45431
Redcamel <webseon@gmail.com>
2015-05-24 21:14:20 +0900

    #8 polygon 최저정점 추출

--- src/Primitive.js
+++ src/Primitive.js
@@ -103,11 +103,9 @@ var Primitive = (function () {
             var vs = [0,0,0,0.5,0.5],is = [],i
             for(i =0; i<n+1; i++){
                 vs.push(mS(perAngle*i),mC(perAngle*i),0,  -mS(perAngle*i)/2+0.5,-mC(perAngle*i)/2+0.5)
-                vs.push(mS(perAngle*(i+1)),mC(perAngle*(i+1)),0,  -mS(perAngle*(i+1))/2+0.5,-mC(perAngle*(i+1))/2+0.5)
             }
             for (i = 0; i < n; i++) {
-                is.push(0, i * 2 + 1, i * 2 + 2),
-                is.push(0, i * 2 + 2, i * 2 + 3)
+                is.push(0, i + 1, i  + 2)
             }
             var result = new Geometry(vs, is, [Vertex.x, Vertex.y, Vertex.z, Vertex.u, Vertex.v]);
             result._key = 'polygon_' + (arguments[0] || 1);
