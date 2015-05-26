d91c3df6e2fa79f8343a272361736f21c3fa13eb
shallaa <shallaa@shallaazm.com>
2015-05-26 13:36:53 +0900

    #250 master src 폴더 추가

---
+++ src/FreeController.js
@@ -0,0 +1,84 @@
+/**
+ * Created by redcamel on 2015-05-22.
+ */
+var FreeController = (function () {
+    var FreeController, fn,MAT1,MAT2;
+    MAT1 = Matrix.create(), MAT2 = Matrix.create(),
+    FreeController = function FreeController(camera) {
+        if(!(camera instanceof Camera)) MoGL.error('FreeController','contructor',0)
+        this._camera = camera
+        this._displacement = [0,0,0]
+        this._speed = 1000.0
+        this._smoothDelay = 0.1
+        this._desirePosition=this._camera.getPosition()
+    },
+    fn = FreeController.prototype,
+    fn.autoBindKey = function(){
+        //TODO 기본 조작키를 세팅하자..c
+    }
+    fn.bindKey = function(){
+        //TODO 전후좌우 로테이션등의 키를 바인딩할수있게 개선
+    },
+    fn.setSpeed = function setSpeed(value){
+        this._speed = value
+    },
+    fn.setSmoothDelay = function setSmoothDelay(value){
+        this._smoothDelay = value > 0.5 ? 0.5 : value
+    },
+    fn.getSpeed = function getSpeed(){
+         return this._speed
+    },
+    fn.getSmoothDelay = function getSmoothDelay(){
+        return this._smoothDelay
+    },
+    fn.update = function update(){
+        var forward = false, back = false, up = false, down = false, left = false, right = false;
+        var updateRotateX,updateRotateY,updateRotateZ,speed;
+        var downKey,tCamera;
+        updateRotateX = updateRotateY = updateRotateZ = 0,
+        downKey = KeyBoard.downed,
+        tCamera = this._camera
+        if (downKey[KeyBoard.A]) left = true
+        if (downKey[KeyBoard.D]) right = true
+        if (downKey[KeyBoard.R]) up = true
+        if (downKey[KeyBoard.F]) down = true
+        if (downKey[KeyBoard.W]) forward = true
+        if (downKey[KeyBoard.S]) back = true
+
+        speed = this._speed*0.001
+
+        if (downKey[KeyBoard.Q]) updateRotateY = 0.01 * speed
+        if (downKey[KeyBoard.E]) updateRotateY = -0.01 * speed
+        if (downKey[KeyBoard.T]) updateRotateX = 0.01 * speed
+        if (downKey[KeyBoard.G]) updateRotateX = -0.01 * speed
+
+
+        this._displacement[0] = right ? -speed : (left ? speed : 0),
+        this._displacement[1] = up ? -speed : (down ? speed : 0),
+        this._displacement[2] = forward ? -speed : (back ? speed : 0)
+
+        Matrix.identity(MAT1),
+        Matrix.translate(MAT1, MAT1, this._displacement),
+        Matrix.identity(MAT2),
+        Matrix.rotateZ(MAT2, MAT2, tCamera.rotateZ),
+        Matrix.rotateY(MAT2, MAT2, tCamera.rotateY),
+        Matrix.rotateX(MAT2, MAT2, tCamera.rotateX),
+        Matrix.multiply(MAT1, MAT2, MAT1),
+
+        this._desirePosition[0]+=MAT1[12]
+        this._desirePosition[1]+=MAT1[13]
+        this._desirePosition[2]+=MAT1[14]
+
+        tCamera.x += (this._desirePosition[0] - tCamera.x)*this._smoothDelay,
+        tCamera.y += (this._desirePosition[1] - tCamera.y)*this._smoothDelay,
+        tCamera.z += (this._desirePosition[2] - tCamera.z)*this._smoothDelay,
+        tCamera.rotateX += updateRotateX,
+        tCamera.rotateY += updateRotateY ,
+        this._displacement[0]=0,
+        this._displacement[1]=0,
+        this._displacement[2]=0,
+        updateRotateX = 0,
+        updateRotateY = 0
+    }
+    return MoGL.ext(FreeController, MoGL);
+})();
---
+++ src/Geometry.js
@@ -0,0 +1,102 @@
+/**
+ * Created by redcamel on 2015-05-05.
+ * description
+ 정점배열과 인덱스 배열을 이용하여 기하구조를 정의함.
+ 생성자에서 지정된 버퍼 및 정보는 변경불가로 생성 이후는 읽기만 가능함.
+ */
+var Geometry = (function () {
+    //그중에 자신의 4좌표랑 7uv랑 8rgba랑 9노말은 지오메트리거고
+    var Geometry, fn;
+    Geometry = function Geometry(vertex, index, info) {
+        var i, len, t, t2,
+        isFloat32 = vertex instanceof Float32Array,
+        isUint16 = index instanceof Uint16Array
+        if (!(Array.isArray(vertex) || isFloat32 )) MoGL.error('Geometry', 'constructor', 0)
+        if (!(Array.isArray(index) || isUint16  )) MoGL.error('Geometry', 'constructor', 1)
+        if (info) {
+            i = info.length
+            if(vertex.length % i) MoGL.error('Geometry', 'constructor', 2)
+            while(i--) info[info[i]] = i
+            //console.log(info)
+        }
+        /////////////////////////////////////
+        t = arguments[2] ? arguments[2].length : 3
+        this._vertexCount = vertex.length / t,
+        this._triangleCount = index.length / 3,
+        this._vertexShaders = {},
+        this._position = [],
+        this._normal = [],
+        this._uv = [],
+        this._color = [],
+        this._volume=null,
+        this._key = null
+        ///////////////////////////////
+        //TODO 노말,UV,컬러없을떄 판별
+        if (arguments[2]) {
+            for (i = 0, len = vertex.length / t; i < len; i++) {
+                t2 = t * i,
+                this._position.push(vertex[t2 + info.x], vertex[t2 + info.y], vertex[t2 + info.z]),
+                info.nx ? this._normal.push(vertex[t2 + info.nx], vertex[t2 + info.ny], vertex[t2 + info.nz]) : 0,
+                info.u ? this._uv.push(vertex[t2 + info.u], vertex[t2 + info.v]) : 0,
+                info.r ? this._color.push(vertex[t2 + info.r], vertex[t2 + info.g], vertex[t2 + info.b], vertex[t2 + info.a]) : 0
+            }
+            this._position = new Float32Array(this._position),
+            this._uv = new Float32Array(this._uv),
+            this._color = new Float32Array(this._color)
+        } else this._position = isFloat32 ? vertex : new Float32Array(vertex)
+        //TODO Uint32Array을 받아줄것인가! 고민해야됨..
+        this._index = isUint16 ? index : new Uint16Array(index)
+        if(this._normal.length==0) this._normal = new Float32Array(calculateNormals(this._position, this._index))
+        else this._normal = new Float32Array(this._normal)
+        ///////////////////////////////
+    }
+    var calculateNormals = function calculateNormals(v, i) {
+        var x = 0, y = 1, z = 2, j, k, len, mSqt = Math.sqrt, ns = [], v1 = [], v2 = [], n0 = [], n1 = [];
+        for (j = 0, len = v.length; j < len; j++) ns[j] = 0.0;
+        for (j = 0, len = i.length; j < len; j = j + 3) {
+            v1 = [], v2 = [], n0 = [], v1[x] = v[3 * i[j + 1] + x] - v[3 * i[j] + x], v1[y] = v[3 * i[j + 1] + y] - v[3 * i[j] + y], v1[z] = v[3 * i[j + 1] + z] - v[3 * i[j] + z], v2[x] = v[3 * i[j + 2] + x] - v[3 * i[j + 1] + x], v2[y] = v[3 * i[j + 2] + y] - v[3 * i[j + 1] + y], v2[z] = v[3 * i[j + 2] + z] - v[3 * i[j + 1] + z], n0[x] = v1[y] * v2[z] - v1[z] * v2[y], n0[y] = v1[z] * v2[x] - v1[x] * v2[z], n0[z] = v1[x] * v2[y] - v1[y] * v2[x];
+            for (k = 0; k < 3; k++) ns[3 * i[j + k] + x] = ns[3 * i[j + k] + x] + n0[x], ns[3 * i[j + k] + y] = ns[3 * i[j + k] + y] + n0[y], ns[3 * i[j + k] + z] = ns[3 * i[j + k] + z] + n0[z]
+        }
+        for (var i = 0, len = v.length; i < len; i = i + 3) {
+            n1 = [], n1[x] = ns[i + x], n1[y] = ns[i + y], n1[z] = ns[i + z];
+            var len = mSqt((n1[x] * n1[x]) + (n1[y] * n1[y]) + (n1[z] * n1[z]));
+            if (len == 0) len = 0.00001;
+            n1[x] = n1[x] / len, n1[y] = n1[y] / len, n1[z] = n1[z] / len, ns[i + x] = n1[x], ns[i + y] = n1[y], ns[i + z] = n1[z];
+        }
+        return ns;
+    }
+    fn = Geometry.prototype,
+    fn.addVertexShader = function addVertexShader(id) { MoGL.isAlive(this);
+        // TODO 마일스톤0.5
+        this._vertexShaders[id] = id
+        return this
+    },
+    fn.getVertexCount = function getVertexCount() { MoGL.isAlive(this);
+        return this._vertexCount
+    },
+    fn.getTriangleCount = function getTriangleCount() { MoGL.isAlive(this);
+        return this._triangleCount
+    },
+    fn.getVolume = function getVolume() { MoGL.isAlive(this);
+        if (!this._volume) {
+            var minX = 0, minY = 0, minZ = 0, maxX = 0, maxY = 0, maxZ = 0
+            var t0, t1, t2, t = this._position, i = t.length
+            while (i--) {
+                t0 = i * 3, t1 = t0 + 1, t2 = t0 + 2
+                minX = t[t0] < minX ? t[t0] : minX,
+                maxX = t[t0] > maxX ? t[t0] : maxX,
+                minY = t[t1] < minY ? t[t1] : minY,
+                maxY = t[t1] > maxY ? t[t1] : maxY,
+                minZ = t[t2] < minZ ? t[t2] : minZ,
+                maxZ = t[t2] > maxZ ? t[t2] : maxZ
+            }
+            this._volume = [maxX - minX, maxY - minY, maxZ - minZ]
+        }
+        return this._volume
+    },
+    fn.removeVertexShader = function removeVertexShader(id) { MoGL.isAlive(this);
+        // TODO 마일스톤0.5
+        return delete this._vertexShaders[id], this
+    }
+    return MoGL.ext(Geometry, MoGL);
+})();
